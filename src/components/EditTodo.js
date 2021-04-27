0371198029346b87a44fc59a906339dc959ec38c
Gabriel Ballester <gaby.ballester@gmail.com>
2021-04-28 01:46:18 +0200

    Define EditTodo + modal

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,5 +1,43 @@
+import { Fragment } from 'react';
+
 export default function EditTodo() {
   return (
-    <h1 className="btn btn-warning">Edit</h1>
+    <Fragment>
+      {/* Trigger the modal with a button */}
+      <button type="button" className="btn btn-warning btn-lg"
+        data-toggle="modal" data-target="#myModal">
+        Edit</button>
+
+      {/* Modal */}
+      <div id="myModal" className="modal fade" role="dialog">
+        <div className="modal-dialog">
+
+          {/* Modal content */}
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todo</h4>
+              <button type="button" className="close"
+                data-dismiss="modal">&times;</button>
+            </div>
+
+            <div className="modal-body">
+              <input type="text" className="form-control" />
+            </div>
+
+            <div className="modal-footer">
+              <button type="button" className="btn btn-warning"
+                data-dismiss="modal">
+                Save
+              </button>
+              <button type="button" className="btn btn-danger"
+                data-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </Fragment>
   )
 }
--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -41,7 +41,7 @@ export default function ListTodos() {
           {todos.map(todo => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td><EditTodo /></td>
+              <td> <EditTodo todo={todo} /></td>
               <td><button
                 className="btn btn-danger"
                 onClick={() => deleteTodo(todo.todo_id)}
