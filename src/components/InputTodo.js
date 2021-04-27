8fc66a280c705111d720ee1bd5452a5144e9b286
Gabriel Ballester <gaby.ballester@gmail.com>
2021-04-27 23:51:28 +0200

    Add InputTodo

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
+import { saveTodoApi } from '../services/api.service';
+
 const InputTodo = () => {
+  const [description, setDescription] = useState("hello");
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    saveTodoApi(description)
+  };
+
   return (
-    <h1>Input Todo</h1>
+    <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+      <input
+        type="text"
+        className="form-control"
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <button className="btn btn-success">Add</button>
+    </form>
   )
 }
 export default InputTodo;
