34876d5857a1ff003665342cda35271555011e70
Gabriel Ballester <gaby.ballester@gmail.com>
2021-04-28 02:26:57 +0200

    Fix minor bugs

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -23,15 +23,23 @@ export default function EditTodo({ todo }) {
         Edit</button>
 
       {/* Modal */}
-      <div className="modal fade" id={`id${todo.todo_id}`}>
+      <div
+        className="modal fade"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
         <div className="modal-dialog">
 
           {/* Modal content */}
           <div className="modal-content">
             <div className="modal-header">
               <h4 className="modal-title">Edit Todo</h4>
-              <button type="button" className="close"
-                data-dismiss="modal">&times;</button>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >&times;</button>
             </div>
 
             <div className="modal-body">
@@ -52,8 +60,12 @@ export default function EditTodo({ todo }) {
               >
                 Save
               </button>
-              <button type="button" className="btn btn-danger"
-                data-dismiss="modal">
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
                 Close
               </button>
             </div>
